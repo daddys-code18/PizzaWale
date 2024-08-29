c261cf6256ef3fe617ba1e9a017dfa364aa23d44
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-08-30 04:45:04 +0530

    Cart Delete  And Increment  Functonality

--- src/utils/ContextReducer.js
+++ src/utils/ContextReducer.js
@@ -15,14 +15,52 @@ const reducer = (state, action) => {
           img: action.img,
         },
       ];
-      case "UPDATE":
-        let arr =[...state];
-        arr.find((food,index)=>{
-            if(food.tempId ===action.tempId){
-                arr[index]={...food,gty:parseInt(action.qty) +parseInt(food.qty),price:action.price+food.price}
-            }
-        })
-        return arr
+    case "UPDATE":
+      let arr = [...state];
+      arr.find((food, index) => {
+        if (food.tempId === action.tempId) {
+          arr[index] = {
+            ...food,
+            gty: parseInt(action.qty) + parseInt(food.qty),
+            price: action.price + food.price,
+          };
+        }
+      });
+      return arr;
+    case "REMOVE":
+      let newArr = [...state];
+      newArr.splice(action.index, 1);
+      return newArr;
+      
+    case "INCREMENT":
+      let incArr = [...state];
+      incArr.find((food, index) => {
+        if (food.tempId === action.tempId) {
+          incArr[index] = {
+            ...food,
+            qty: parseInt(food.qty) + 1,
+            price: food.price + action.unitPrice,
+          };
+        }
+      });
+      return incArr;
+
+    case "DECREMENT":
+      let decArr = [...state];
+      decArr.find((food, index) => {
+        if (food.tempId === action.tempId) {
+          decArr[index] = {
+            ...food,
+            qty: parseInt(food.qty) - 1,
+            price: food.price - action.unitPrice,
+          };
+        }
+      });
+      return decArr;
+
+    case "DROP":
+      let emptyArr = [];
+      return emptyArr;
     default:
       console.log("Action type");
   }
