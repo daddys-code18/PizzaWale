a56dc11fc36e94b86c1e1868b2569e4d809295d1
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-08-30 15:23:08 +0530

    Data Filteration  Functonality

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Card from "@/components/home/Card";
 import CarouselComponent from "@/components/home/Carousel";
 import { Inter } from "next/font/google";
 import CardData from "../store/cardData.json";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   let categories = new Set();
   let categoryArray;
+  const [typeFilter, setTypeFilter] = useState(false);
   const foodData = [];
   const handleData = () => {
     CardData.map((data) => {
@@ -22,6 +24,50 @@ export default function Home() {
     <>
       <CarouselComponent />
       <div className="container mx-auto">
+        <div className="my-6 space-x-5">
+          <button
+            className={`border-black rounded-full dark:border-white border-2 py-1 px-3 ${
+              !typeFilter && "bg-slate-300 dark:bg-slate-600"
+            } `}
+            onClick={() => setTypeFilter(false)}
+          >
+            All
+          </button>
+          <button
+            className={`border-black rounded-full dark:border-white border-2 py-1 px-3 ${
+              typeFilter === "Veg" && "bg-slate-300 dark:bg-slate-600"
+            } `}
+            onClick={() => {
+              setTypeFilter("Veg");
+            }}
+          >
+            <span
+              className={
+                "lowercase font-thin bg-white border-green-500 border mr-2 px-0.1 text-green-500"
+              }
+            >
+              ●
+            </span>
+            Veg
+          </button>
+          <button
+            className={`border-black rounded-full dark:border-white border-2 py-1 px-3 ${
+              typeFilter === "Non-Veg" && "bg-slate-300 dark:bg-slate-600"
+            } `}
+            onClick={() => {
+              setTypeFilter("Non-Veg");
+            }}
+          >
+            <span
+              className={
+                "lowercase font-thin bg-white border-red-500 border mr-2 px-0.1 text-red-500"
+              }
+            >
+              ●
+            </span>
+            Non Veg
+          </button>
+        </div>
         {categoryArray.map((category) => {
           return (
             <>
@@ -32,8 +78,11 @@ export default function Home() {
               <div className="flex flex-col items-center justify-center">
                 <div className="grid mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                   {foodData
-                    .filter((foodData) => category === foodData.category)
-                    .map((data) => {
+                    ?.filter((foodData) => category === foodData.category)
+                    ?.filter((foodData) =>
+                      typeFilter ? typeFilter === foodData.foodType : foodData
+                    )
+                    ?.map((data) => {
                       return <Card key={data.name} foodData={data} />;
                     })}
                 </div>
